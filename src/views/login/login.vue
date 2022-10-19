5446600c672a2e5caa1905e1f946926ebfe1c32b
wxg <wxg@wxg.com>
2022-10-19 11:50:17 +0800

    项目初始化以及登录表单校验

---
+++ src/views/login/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    class="demo-ruleForm"
+  >
+    <el-form-item prop="username">
+      <el-input v-model="ruleForm.username" type="text" autocomplete="off" />
+    </el-form-item>
+    <el-form-item prop="pwd">
+      <el-input v-model="ruleForm.pwd" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="loginFn()">登录</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang='ts' setup>
+import { reactive, toRefs, ref } from "vue";
+const state = reactive({
+  ruleForm: {
+    username: "",
+    pwd: "",
+  },
+});
+const validatePwd = (
+  rule: unknown,
+  value: string | undefined,
+  cb: (msg?: string) => void
+) => {
+  if (!value) {
+    cb("密码不能为空");
+  } else {
+    cb();
+  }
+};
+// 校验规则
+const rules = reactive({
+  username: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+  pwd: [{ validator: validatePwd, trigger: "blur" }],
+});
+let { ruleForm } = toRefs(state);
+const loginFn = () => {};
+</script>
+<style lang='less' scoped>
+</style>
